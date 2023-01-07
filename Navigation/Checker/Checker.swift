420015b60e6788e9dd0c146a634d2bbc79040a99
Artem Zaytsev <101665702+temaathletic@users.noreply.github.com>
2023-01-08 00:05:54 +0300

    "Структурные паттерны: Delegate, Singleton, Factory"

---
+++ Navigation/Checker/Checker.swift
@@ -0,0 +1,8 @@
+//
+//  Checker.swift
+//  Navigation
+//
+//  Created by temaathletic on 07.01.2023.
+//
+
+import Foundation
---
+++ Navigation/Checker/LoginFactory.swift
@@ -0,0 +1,8 @@
+//
+//  LoginFactory.swift
+//  Navigation
+//
+//  Created by temaathletic on 07.01.2023.
+//
+
+import Foundation
